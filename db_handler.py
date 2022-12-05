c9d180c34ebab6a4846619a53424fde92b9f1945
Emily-Hickey <msemilymhickey@gmail.com>
2022-12-05 14:38:45 -0500

    First Commit (Part two) - Failed to commit the aforementioned database handler

---
+++ db_handler.py
@@ -0,0 +1,14 @@
+import sqlite3
+from typing import Tuple
+
+def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    # opens the connection to the database
+    db_connection = sqlite3.connect(filename)  # connect to existing DB or create new one
+    open_cursor = db_connection.cursor()  # get ready to read/write data
+    return db_connection, open_cursor
+
+
+def close_db(connection: sqlite3.Connection):
+    # closes the connection to the database
+    connection.commit()  # make sure any changes get saved
+    connection.close()
