6522e6cafc9bfcf90c8538b7d52fd034b92f882d
Emily-Hickey <msemilymhickey@gmail.com>
2022-12-08 11:38:50 -0500

    Second Commit - Dr. Jung clarified the requirements of the assignment. Will generate our own database

--- db_handler.py
+++ db_handler.py
@@ -1,6 +1,7 @@
 import sqlite3
 from typing import Tuple
 
+
 def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
     # opens the connection to the database
     db_connection = sqlite3.connect(filename)  # connect to existing DB or create new one
@@ -12,3 +13,8 @@ def close_db(connection: sqlite3.Connection):
     # closes the connection to the database
     connection.commit()  # make sure any changes get saved
     connection.close()
+
+
+def interrupt_exterior_script(filename: str):
+    # reads an .sql file and applies it to the database
+    print()
