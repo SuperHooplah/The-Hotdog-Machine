c788f6a95b8ed202aab31057508a594b255a8c79
shawn <s1lovell@student.bridgew.edu>
2022-12-19 17:11:29 -0500

    Updated open_db This will now check to see if the db has already been created

--- db_handler.py
+++ db_handler.py
@@ -1,22 +1,32 @@
 import sqlite3
+import os
 from typing import Tuple
 
 
-# this method creates a table based off an SQL file
+# Creates a table based the SQL file
 def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
-    # opens the connection to the database and sets up a cursor
-    db_connection = sqlite3.connect(filename + ".db")  # connect to existing DB or create new one
-    open_cursor = db_connection.cursor()  # get ready to read/write data
+    # check if the database file already exists
+    if not os.path.exists(filename + ".db"):
+        # open the connection to the database and sets up a cursor
+        db_connection = sqlite3.connect(filename + ".db")  # create new DB
+        open_cursor = db_connection.cursor()  # get ready to read/write data
 
-    # populate tables based off sql
-    with open(filename + ".sql") as f:
-        # read the SQL statements from the script
-        sql = f.read()
+        # populate tables based off sql
+        with open(filename + ".sql") as f:
+            # read the SQL statements from the script
+            sql = f.read()
 
-    # execute the SQL statements
-    open_cursor.executescript(sql)
+        # execute the SQL statements
+        open_cursor.executescript(sql)
+    else:
+        # if the .db file is already there connect to the existing database and sets up a cursor
+        db_connection = sqlite3.connect(filename + ".db")
+        open_cursor = db_connection.cursor()
 
     return db_connection, open_cursor
+
+
+
 
 
 def close_db(connection: sqlite3.Connection):
