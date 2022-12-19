06eab9d3edd80bbe642df79c637b5d35b80eb380
shawn <s1lovell@student.bridgew.edu>
2022-12-19 17:29:01 -0500

    Removed Price again

--- HotDogSandwich.sql
+++ HotDogSandwich.sql
@@ -26,7 +26,6 @@ CREATE TABLE IF NOT EXISTS "meats" (
 CREATE TABLE IF NOT EXISTS "condiment" (
 	"conID"	INTEGER,
 	"calories"	INTEGER NOT NULL,
-	"price"	DECIMAL NOT NULL,
 	"desc"	TEXT,
 	PRIMARY KEY("conID")
 );
@@ -34,8 +33,8 @@ INSERT INTO "buns" VALUES (1,400,'Made from white Bread');
 INSERT INTO "buns" VALUES (2,350,'Made from wheat Bread');
 INSERT INTO "meats" VALUES (1,350,'Pork');
 INSERT INTO "meats" VALUES (2,300,'Turkey');
-INSERT INTO "condiment" VALUES (1,10,1,'Mustard');
-INSERT INTO "condiment" VALUES (2,12,1,'ketchup');
+INSERT INTO "condiment" VALUES (1,10,'Mustard');
+INSERT INTO "condiment" VALUES (2,12,'ketchup');
 
 INSERT INTO "HotDog" ("dogID", "name", "bun", "meat", "condiments", "story")
 VALUES (1, 'Classic Hot Dog', 1, 1, 1, 'A classic hot dog with a bun made from white bread, pork meat, and mustard condiment.');
