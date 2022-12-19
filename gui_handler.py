728f86dbef00539a5a9fe340d18572886f988b48
Emily-Hickey <msemilymhickey@gmail.com>
2022-12-19 17:33:33 -0500

    Commit six
    -Began working on the Hot Dog Display Window

--- gui_handler.py
+++ gui_handler.py
@@ -6,6 +6,12 @@ def displayRandomHotDog(cursor, text):
     text.delete("1.0", "end")
     text.insert("insert", str(random.randint(0, 10)))
 
+
+def displayHotDogsWindow(cursor, window):
+    newWindow = tk.Toplevel(window)
+    newWindow.title("Hot Dog Table")
+    tk.Label(newWindow, text = "This is a new window").pack()
+
 # Used to start a gui with tkinter as tk. Only displays content of db currently
 def gui(cursor):
     # create a new window
@@ -16,18 +22,21 @@ def gui(cursor):
     # Create left and right frames
 
     # the right frame will hold the text that gets updated with each button press
-    right_frame = tk.Frame(root, bg='grey')
+    right_frame = tk.Frame(root)
     right_frame.pack(side='right')
 
     text = tk.Text(right_frame)
     text.pack()
 
     # The left frame will hold all of the button for our functions
-    left_frame = tk.Frame(root, width=200, height=400, bg='grey')
+    left_frame = tk.Frame(root, width=200, height=400,)
     left_frame.pack(side='left')
 
     genHotDog = tk.Button(left_frame, text="Generate Hot Dog", command=lambda: displayRandomHotDog(cursor, text))
-    genHotDog.pack()
+    genHotDog.pack(side='top')
+
+    displayAllDogs = tk.Button(left_frame, text="See all Hot Dogs", command=lambda: displayHotDogsWindow(cursor, root))
+    displayAllDogs.pack(side='bottom')
 
     # run the main event loop
     # This will process events to update the user interface allowing user input
