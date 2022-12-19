43806f7045bd833def78e456292b7e1a900bd378
shawn <s1lovell@student.bridgew.edu>
2022-12-19 16:39:28 -0500

    Changed the sql file

--- HotDogSandwich.sql
+++ HotDogSandwich.sql
@@ -1,24 +1,39 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "HotDog" (
-	"Bun"	INTEGER,
-	"Condiments"	INTEGER,
-	"Toppings"	INTEGER,
-	"Meat"	INTEGER
+	"dogID"	INTEGER,
+	"name"	VARCHAR(255) NOT NULL,
+	"bun"	INTEGER NOT NULL,
+	"meat"	INTEGER NOT NULL,
+	"condiments"	INTEGER NOT NULL,
+	"story"	TEXT,
+	FOREIGN KEY("meat") REFERENCES "meats"("meatID"),
+	FOREIGN KEY("condiments") REFERENCES "condiment"("conID"),
+	FOREIGN KEY("bun") REFERENCES "buns"("bunID"),
+	PRIMARY KEY("dogID")
 );
-CREATE TABLE IF NOT EXISTS "Sandwich" (
-	"Bread"	INTEGER,
-	"Meat"	INTEGER,
-	"Condiments"	INTEGER,
-	"Toppings"	INTEGER
+CREATE TABLE IF NOT EXISTS "buns" (
+	"bunID"	INTEGER,
+	"calories"	INTEGER NOT NULL,
+	"desc"	TEXT,
+	PRIMARY KEY("bunID")
 );
-INSERT INTO "HotDog" VALUES ('Classic hot dog bun','Mustard','Onion','Chicken hot dog');
-INSERT INTO "HotDog" VALUES ('Pretzel bun','Ketchup','Sauerkraut','Vegetarian hot dog');
-INSERT INTO "HotDog" VALUES ('Wheat hot dog bun','Relish','Pickle slices','Turkey hot dog');
-INSERT INTO "HotDog" VALUES ('Bagel bun','Mayonnaise','Jalapeno peppers','Pork hot dog');
-INSERT INTO "HotDog" VALUES ('Poppyseed bun','Barbecue Sauce','Cheese','Beef hot dog');
-INSERT INTO "Sandwich" VALUES ('White Bread','Turkey','Mayonnaise','Lettus');
-INSERT INTO "Sandwich" VALUES ('Whole wheat bread','Ham','Mustard','Cheese');
-INSERT INTO "Sandwich" VALUES ('Rye bread','Roast beef','Ketchup','Tomato');
-INSERT INTO "Sandwich" VALUES ('Pita bread','Tuna','Relish','Avocodo');
-INSERT INTO "Sandwich" VALUES ('Flatbread','Chicken','Olive oil','Onion');
+CREATE TABLE IF NOT EXISTS "meats" (
+	"meatID"	INTEGER,
+	"calories"	INTEGER NOT NULL,
+	"desc"	TEXT,
+	PRIMARY KEY("meatID")
+);
+CREATE TABLE IF NOT EXISTS "condiment" (
+	"conID"	INTEGER,
+	"calories"	INTEGER NOT NULL,
+	"price"	DECIMAL NOT NULL,
+	"desc"	TEXT,
+	PRIMARY KEY("conID")
+);
+INSERT INTO "buns" VALUES (1,400,'Made from white Bread');
+INSERT INTO "buns" VALUES (2,350,'Made from wheat Bread');
+INSERT INTO "meats" VALUES (1,350,'Pork');
+INSERT INTO "meats" VALUES (2,300,'Turkey');
+INSERT INTO "condiment" VALUES (1,10,1,'Mustard');
+INSERT INTO "condiment" VALUES (2,12,1,'ketchup');
 COMMIT;
