dbd18de73f356fd154438dc9a7ea6bf0db202172
shawn <s1lovell@student.bridgew.edu>
2022-12-19 19:43:56 -0500

    -Build addIngredientToTable
    -Added buttons for adding buns, meats, condiments to the db using addIngredientToTable

--- gui_handler.py
+++ gui_handler.py
@@ -1,7 +1,12 @@
 import tkinter as tk
+# Simpledialog user for user input
+from tkinter import simpledialog
 import random
 
 # this function pulls data from the database and generates a random hot dog
+import db_handler
+
+
 def displayRandomHotDog(cursor, text):
     text.delete("1.0", "end")
     text.insert("insert", str(random.randint(0, 10)))
@@ -17,9 +22,44 @@ def displayHotDogsWindow(cursor, window):
     # query the HotDog table and display the results in the text widget
     hotdog_text.insert("insert", "PUT TEXT OUTPUT HERE")
 
-def addIngredientToTable(cursor, type):
-    # check what type of ingredient, open that table and add item
-    print("")
+def addIngredientToTable(type):
+
+    # create a new window
+    name = tk.simpledialog.askstring("Name", "Please enter the name to add?")
+    # Get the calorie amount
+    calories = tk.simpledialog.askinteger("Calories", "Please enter the amount of calories to add?")
+
+    # Open the db
+    db, cursor = db_handler.open_db("HotDogSandwich")
+    c = cursor
+
+    # Adds info from the user to db based on button selection
+    if type == "condiment":
+        c.execute("SELECT COUNT(*) FROM condiment")
+        hold = c.fetchone()
+        new_item_key = hold[0]+1
+
+        c.execute(" INSERT INTO condiment (conID, name, calories) VALUES (?, ?, ?)",
+                  (new_item_key, name, calories))
+        db.commit()
+
+    elif type == "meat":
+        c.execute("SELECT COUNT(*) FROM meats")
+        hold = c.fetchone()
+        new_item_key = hold[0]+1
+
+        c.execute(" INSERT INTO meats (meatID, name, calories) VALUES (?, ?, ?)",
+                  (new_item_key, name, calories))
+        db.commit()
+
+    elif type == "bun":
+        c.execute("SELECT COUNT(*) FROM buns")
+        hold = c.fetchone()
+        new_item_key = hold[0]+1
+
+        c.execute(" INSERT INTO buns (bunID, name, calories) VALUES (?, ?, ?)",
+                  (new_item_key, name, calories))
+        db.commit()
 
 # Used to start a gui with tkinter as tk. Only displays content of db currently
 def gui(cursor):
@@ -41,12 +81,23 @@ def gui(cursor):
     left_frame = tk.Frame(root, width=200, height=400,)
     left_frame.pack(side='left')
 
+
+
     genHotDog = tk.Button(left_frame, text="Generate Hot Dog", command=lambda: displayRandomHotDog(cursor, text))
     genHotDog.pack(side='top')
 
     addCondimentBTN = tk.Button(left_frame, text="Add Condiment",
-                                command=lambda: addIngredientToTable(cursor, "condiment"))
+                                command=lambda: addIngredientToTable("condiment"))
+
+    addMeatBTN = tk.Button(left_frame, text="Add Meat Type",
+                                command=lambda: addIngredientToTable("meat"))
+
+    addBunBTN = tk.Button(left_frame, text="Add Bun Type",
+                                command=lambda: addIngredientToTable("bun"))
+
     addCondimentBTN.pack(side='top')
+    addMeatBTN.pack(side='top')
+    addBunBTN.pack(side='top')
 
     displayAllDogs = tk.Button(left_frame, text="See all Hot Dogs", command=lambda: displayHotDogsWindow(cursor, root))
     displayAllDogs.pack(side='bottom')
